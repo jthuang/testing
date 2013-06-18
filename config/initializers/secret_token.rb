39a37c4c4d84e657fc784636fd9d47f5ebd2c37e
JT Huang <jthuang@ischool.berkeley.edu>
2013-06-18 15:35:23 -0700

    Initial commit with Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodlawProd::Application.config.secret_token = '80eac96c9c9530316225bbe5ad2656a99ae496cd608b7edeffe513dc1035b873f2e91ddd1f2a621015a9f3cae749bd7ac957850d69ecb3bfdd134fba02d6dfd9'
